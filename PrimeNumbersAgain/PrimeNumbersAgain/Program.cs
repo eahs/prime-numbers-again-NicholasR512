4b0b117ac1b87c04190d4efe5a8878161c2e8611
Nicholas Ressler <resslern@roverkids.org>
2024-09-22 10:44:56 -0400

    adds FindnthPrime functionality, adds FindPrime, and adds isPrime

--- PrimeNumbersAgain/PrimeNumbersAgain/Program.cs
+++ PrimeNumbersAgain/PrimeNumbersAgain/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading;
 
 namespace PrimeNumbersAgain
 {
@@ -25,7 +27,71 @@ namespace PrimeNumbersAgain
 
         static int FindNthPrime(int n)
         {
-            return 0;
+            if (n <= 250000)
+            {
+                return FindPrime(n, 1, 2, 3);
+            }
+            else if (n <= 500000)
+            {
+                return FindPrime(n, 250000, 3497861, 3497867);
+            }
+            else if (n <= 750000)
+            {
+                return FindPrime(n, 500000, 7368787, 7368791);
+            }
+            else if (n <= 1000000)
+            {
+                return FindPrime(n, 750000, 11381621, 11381633);
+            }
+            else if (n <= 1250000)
+            {
+                return FindPrime(n, 1000000, 15485863, 15485867);
+            }
+            else if (n <= 1500000)
+            {
+                return FindPrime(n, 1250000, 19654991, 19655021);
+            }
+            else if (n <= 1750000)
+            {
+                return FindPrime(n, 1500000, 23879519, 23879539);
+            }
+            else
+            {
+                return FindPrime(n, 1750000, 28146689, 28146691);
+            }
+        }
+
+        static int FindPrime(int n, int count, int firstPrime, int nextPrime)
+        {
+            int currentPrime = firstPrime;
+
+            for (int i = nextPrime; count < n; i += 2)
+            {
+                if (IsPrime(i))
+                {
+                    currentPrime = i;
+                    count++;
+                }
+            }
+
+            return currentPrime;
+        }
+
+        static bool IsPrime(int number)
+        {
+            if (number < 2) { return false; }
+            if (number == 2) { return true; }
+            if (number % 2 == 0) { return false; }
+
+            for (int j = 3; j <= Math.Sqrt(number); j += 2) // Check only odd divisors
+            {
+                if (number % j == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         static int GetNumber()
@@ -54,7 +120,7 @@ namespace PrimeNumbersAgain
             Console.WriteLine(".##......##..##....##....##...##..##..........##.");
             Console.WriteLine(".##......##..##..######..##...##..######...####..");
             Console.WriteLine(".................................................\n\n");
-            Console.WriteLine("Nth Prime Solver O-Matic Online..\nGuaranteed to find primes up to 2 million in under 30 seconds!\n\n");
+            Console.WriteLine("Nth Prime Solver O-Matic Online..\nGuaranteed to find primes up to 2 million in under 3 seconds!\n\n");
             
         }
 
@@ -63,7 +129,7 @@ namespace PrimeNumbersAgain
             Console.WriteLine("\n");
             Console.Write("Time Check: ");
 
-            if (time <= 10)
+            if (time <= 3)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Pass");
